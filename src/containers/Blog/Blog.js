450d3d7d74397c3f3e5b52cd046641c53e68918b
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-05-07 13:49:26 -0700

    fetching data, setting a post and delete route and handling errors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,9 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -23,20 +25,36 @@ class Blog extends Component {
                 })
                 this.setState({posts: updatedPosts});
                 //console.log(response);
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error: true})
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
-        const posts = this.state.posts.map( post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
-        });
+        // here we are greating a new let posts that handles the error from the state that is false at the launch
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
+        if(!this.state.error) {
+            posts = this.state.posts.map( post => {
+                return <Post 
+                            key={post.id} 
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}/>
+            });
+        } 
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
