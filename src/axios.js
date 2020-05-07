13ddf08ce2254e6611b85f41348fd3e1c82f3995
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-05-07 15:39:05 -0700

    setting a defaul global config for axios and using axios instances

---
+++ src/axios.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+// instance.interceptors.request...
+
+export default instance;
+
+//local axios state
